dc70f825c15548661ea6b2fadeb4685b4c7be7dc
Rakesh Karmaker <rakeshkarmaker65@gmail.com>
2026-07-13 19:11:35 +0600

    feat: Remove old code.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { amenitiesApi } from '@/lib/api'
+
 import { useQuery } from '@tanstack/react-query'
 import { clsx  } from 'clsx'
 import type {ClassValue} from 'clsx';
@@ -6,18 +6,4 @@ import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
-}
-
-
-export function GetRoomAmenities() {
-    const { data } = useQuery({
-        queryKey: ['amenities', { category: 'room' }],
-        queryFn: () => amenitiesApi.list({ category: 'room' }).catch(() => null),
-    })
-
-    if (!data || !data.data) {
-        return []
-    }
-
-    return data.data
 }
